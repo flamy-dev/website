311f7982d719eabdfdfb9304786b4cdfa9ee2b5f
Sameer Utradhi <sameer@flamy.dev>
2020-12-06 08:11:45 +0530

    carousel for mobile view

--- components/PageCarousel/PageCarousel.tsx
+++ components/PageCarousel/PageCarousel.tsx
@@ -5,13 +5,22 @@ import CarouselElement from "../CarouselElement/CarouselElement";
 
 const PageCarousel = () => {
   return (
-    <Carousel autoPlay showArrows={true} className={`h-auto md:m-10 w-full mt-32 md:mt-0`} infiniteLoop useKeyboardArrows showThumbs={false} swipeable={true} showStatus={false}>
-      {services.map((service) => {
-        return (
-          <CarouselElement key={service.heading} path={service.path} description={service.description} heading={service.heading} />
-        )
-      })}
-    </Carousel>
+    <>
+      <Carousel autoPlay showArrows={true} className={`hidden md:block h-auto md:m-10 w-full mt-32 md:mt-0`} infiniteLoop useKeyboardArrows showThumbs={false} swipeable={true} showStatus={false}>
+        {services.map((service) => {
+          return (
+            <CarouselElement key={service.heading} path={service.path} description={service.description} heading={service.heading} />
+          )
+        })}
+      </Carousel>
+      <div className="md:hidden h-auto w-full mt-32 p-4 ">
+        {services.map((service) => {
+          return (
+            <CarouselElement key={service.heading} path={service.path} description={service.description} heading={service.heading} />
+          )
+        })}
+      </div>
+    </>
   )
 }
 
