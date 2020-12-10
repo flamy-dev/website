136810e9c66a1d296bccc56ed5353c23501d4de9
Meet Mangukiya <meet@flamy.dev>
2020-12-10 11:55:37 +0530

    OurClients: breakout client data into data/clients

--- components/OurClients/OurClients.tsx
+++ components/OurClients/OurClients.tsx
@@ -1,70 +1,28 @@
+import _ from "lodash";
+import clients from "../../data/clients";
+
 const OurClients = () => {
+  const chunkSize = 3;
+
   return (
     <div className="flex flex-col justify-start items-center w-full h-full">
       <div className="flex justify-center items-center text-3xl mb-10 md:text-5xl md:mb-32">
         Our <span className="font-bold">&nbsp; Clients</span>
       </div>
-      <div className="flex flex-wrap items-center justify-center lg:justify-between w-full md:w-client mb-4 md:mb-16 ">
-        <div>
-          <a href="https://www.github.com" target="_blank">
-            <img
-              src="logo.svg"
-              className="w-24 md:w-32 opacity-30 hover:opacity-100 transition-all"
-            />
-          </a>
-          <a href="https://www.github.com" target="_blank">
-            <img
-              src="logo.svg"
-              className="w-24 md:w-32 opacity-30 hover:opacity-100 transition-all"
-            />
-          </a>
-          <a href="https://www.github.com" target="_blank">
-            <img
-              src="logo.svg"
-              className="w-24 md:w-32 opacity-30 hover:opacity-100 transition-all"
-            />
-          </a>
-        </div>
-        <div>
-          <a href="https://www.github.com" target="_blank">
-            <img
-              src="logo.svg"
-              className="w-24 md:w-32 opacity-30 hover:opacity-100 transition-all"
-            />
-          </a>
-          <a href="https://www.github.com" target="_blank">
-            <img
-              src="logo.svg"
-              className="w-24 md:w-32 opacity-30 hover:opacity-100 transition-all"
-            />
-          </a>
-          <a href="https://www.github.com" target="_blank">
-            <img
-              src="logo.svg"
-              className="w-24 md:w-32 opacity-30 hover:opacity-100 transition-all"
-            />
-          </a>
-        </div>
-        <div>
-          <a href="https://www.github.com" target="_blank">
-            <img
-              src="logo.svg"
-              className="w-24 md:w-32 opacity-30 hover:opacity-100 transition-all"
-            />
-          </a>
-          <a href="https://www.github.com" target="_blank">
-            <img
-              src="logo.svg"
-              className="w-24 md:w-32 opacity-30 hover:opacity-100 transition-all"
-            />
-          </a>
-          <a href="https://www.github.com" target="_blank">
-            <img
-              src="logo.svg"
-              className="w-24 md:w-32 opacity-30 hover:opacity-100 transition-all"
-            />
-          </a>
-        </div>
+
+      <div className="flex flex-col flex-wrap items-center justify-center lg:justify-between w-full md:w-client mb-4 md:mb-16 ">
+        {_.chunk(clients, chunkSize).map((clientChunk) => (
+          <div className="flex flex-row">
+            {clientChunk.map(({ link, logoPath }) => (
+              <a href={link} target="_blank">
+                <img
+                  src={logoPath}
+                  className="w-24 md:w-32 opacity-30 hover:opacity-100 transition-all"
+                />
+              </a>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ data/clients.ts
@@ -0,0 +1,6 @@
+export default [
+  { logoPath: "logo.svg", link: "https://github.com/" },
+  { logoPath: "logo.svg", link: "https://github.com/" },
+  { logoPath: "logo.svg", link: "https://github.com/" },
+  { logoPath: "logo.svg", link: "https://github.com/" },
+];
