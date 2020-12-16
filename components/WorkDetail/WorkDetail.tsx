e9673a5238e81083949159e1fde8910c1ba851f9
Sameer Utradhi <sameer@flamy.dev>
2020-12-16 17:53:11 +0530

    work: change modal max height to parent height in desktop view

--- components/WorkDetail/WorkDetail.tsx
+++ components/WorkDetail/WorkDetail.tsx
@@ -2,14 +2,14 @@ const WorkDetail = (props) => {
   const { details } = props;
 
   return (
-    <div className="overflow-y-scroll md:overflow-hidden overflow-x-hidden min-h-screen cursor-default flex justify-center flex-col items-center p-2 w-full border-none rounded-sm text-white bg-black lg:w-work ">
+    <div className="overflow-y-scroll min-h-screen md:h-full md:min-h-0 md:overflow-hidden cursor-default flex justify-center flex-col items-center p-2 w-full border-none rounded-sm text-white bg-black lg:w-work ">
       <div className="w-full flex flex-col md:flex-row">
         <div className="flex flex-col justify-center items-center p-2 w-full md:w-1/4  m-1">
           <h2 className="text-xl md:mb-10">Brand Name</h2>
 
           <div className="flex jusify-center items-center  relative ">
             <img src="logo.svg" className="w-full h-full opacity-50" />
-            <div className="clientLogo rounded-full">
+            <div className="rounded-full">
               <img
                 src={details?.logoPath}
                 alt="client logo"
@@ -32,67 +32,14 @@ const WorkDetail = (props) => {
             Lorem ipsum
           </div>
         </div>
-        <div
-          className="py-10 md:pb-0 overflow-y-scroll flex-col m-1 flex justify-center md:justify-between items-center p-2 flex-grow md:items-stretch md:w-2/4 text-justify md:text-center w-screen h-screen"
-        >
-          <div className="w-full flex justify-center items-center  m-1 p-1">
-            <div className="mr-1 text-xs md:text-base md:w-auto">
+        <div className="overflow-y-scroll flex flex-col m-1  justify-center md:justify-between items-center p-2 md:py-10 flex-grow md:items-stretch md:w-2/4 text-center md:text-center w-full md:h-screen">
+          <div className="w-full flex flex-col-reverse md:flex-row justify-center items-center">
+            <div className="py-2 md:pr-2 text-sm md:text-md md:w-auto">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-              cumque minima facilis eos nesciunt illo ab maxime laborum natus,
-              nobis perspiciatis.
+              cumque minima facilis eos nesciunt.
             </div>
             <div className="w-full flex justify-center items-center">
-              <img src="insta.png" className="w-40" />
-            </div>
-          </div>
-          <div className="w-full flex justify-center items-center  m-1 p-1">
-            <div className="mr-1 text-xs md:text-base md:w-auto">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-              cumque minima facilis eos nesciunt illo ab maxime laborum natus,
-              nobis perspiciatis.
-            </div>
-            <div className="w-full flex justify-center items-center">
-              <img src="insta.png" className="w-40" />
-            </div>
-          </div>
-          <div className="w-full flex justify-center items-center  m-1 p-1">
-            <div className="mr-1 text-xs md:text-base md:w-auto">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-              cumque minima facilis eos nesciunt illo ab maxime laborum natus,
-              nobis perspiciatis.
-            </div>
-            <div className="w-full flex justify-center items-center">
-              <img src="insta.png" className="w-40" />
-            </div>
-          </div>
-          <div className="w-full flex justify-center items-center  m-1 p-1">
-            <div className="mr-1 text-xs md:text-base md:w-auto">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-              cumque minima facilis eos nesciunt illo ab maxime laborum natus,
-              nobis perspiciatis.
-            </div>
-            <div className="w-full flex justify-center items-center">
-              <img src="insta.png" className="w-40" />
-            </div>
-          </div>
-          <div className="w-full flex justify-center items-center  m-1 p-1">
-            <div className="mr-1 text-xs md:text-base md:w-auto">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-              cumque minima facilis eos nesciunt illo ab maxime laborum natus,
-              nobis perspiciatis.
-            </div>
-            <div className="w-full flex justify-center items-center">
-              <img src="insta.png" className="w-40" />
-            </div>
-          </div>
-          <div className="w-full flex justify-center items-center  m-1 p-1">
-            <div className="mr-1 text-xs md:text-base md:w-auto">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-              cumque minima facilis eos nesciunt illo ab maxime laborum natus,
-              nobis perspiciatis.
-            </div>
-            <div className="w-full flex justify-center items-center">
-              <img src="insta.png" className="w-40" />
+              <img src="adidas.png" className="w-40 md:w-3/5 lg:w-80" />
             </div>
           </div>
         </div>
