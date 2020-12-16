c081544748b4fc3b280f121fbb00a3ca32764103
Meet Mangukiya <meet@flamy.dev>
2020-12-16 22:19:54 +0530

    Add loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,24 @@
 import "../styles/globals.css";
+import { useEffect, useState } from "react";
+import { ReactSVG } from "react-svg";
 
 const MyApp = ({ Component, pageProps }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("DOMContentLoaded", () => {
+      setIsLoaded(true);
+    });
+  }, []);
+
   return (
     <>
-      {/* {isLoaderVisible ? (
+      {isLoaded ? null : (
         <div className="flex fixed h-screen w-screen justify-center items-center bg-gray-500">
-          Loading...
+          <ReactSVG src="./logo.svg" renumerateIRIElements={false} />
         </div>
-      ) : ( */}
+      )}
       <Component {...pageProps} />
-      {/* )} */}
     </>
   );
 };
