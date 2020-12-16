76b212a4f235874d8feca90f80fb18f56b555f0b
Meet Mangukiya <meet@flamy.dev>
2020-12-16 16:33:15 +0530

    work: start using rodal instead of custom modal impl

--- components/WorkDetail/WorkDetail.tsx
+++ components/WorkDetail/WorkDetail.tsx
@@ -2,18 +2,20 @@ const WorkDetail = (props) => {
   const { details } = props;
 
   return (
-    <div className="flex justify-between flex-col items-center p-2 w-full border-2 border-white m-2 text-white bg-black bg-opacity-75 lg:w-work ">
+    <div className="overflow-y-scroll md:overflow-hidden overflow-x-hidden min-h-screen cursor-default flex justify-center flex-col items-center p-2 w-full border-none rounded-sm text-white bg-black lg:w-work ">
       <div className="w-full flex flex-col md:flex-row">
         <div className="flex flex-col justify-center items-center p-2 w-full md:w-1/4  m-1">
-          <h2 className="text-xl">Brand Name</h2>
+          <h2 className="text-xl md:mb-10">Brand Name</h2>
 
           <div className="flex jusify-center items-center  relative ">
-            <img src="insta.png" className="w-full h-full opacity-50" />
-            <img
-              src={details.logoPath}
-              alt="client logo"
-              className="absolute w-36 rounded-full bg-blue-200 border-2 border-gray-300 clientlogo"
-            />
+            <img src="logo.svg" className="w-full h-full opacity-50" />
+            <div className="clientLogo rounded-full">
+              <img
+                src={details?.logoPath}
+                alt="client logo"
+                className="absolute w-36 p-4 clientLogo overflow-y-scroll"
+              />
+            </div>
           </div>
           <div className="mt-8 text-3xl font-extrabold text-center">
             Adidas Automatic Checkout
@@ -27,18 +29,19 @@ const WorkDetail = (props) => {
         <div className="flex flex-col justify-center items-center p-2 w-full md:w-1/4  m-1">
           <h3 className="font-extrabold text-xl">Description</h3>
           <div className="flex justify-center items-center pt-4 text-center">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi rem
-            aliquid incidunt voluptatum. Nesciunt molestias, qui excepturi
+            Lorem ipsum
           </div>
         </div>
-        <div className=" overflow-y-scroll flex-col m-1 flex justify-between p-2 flex-grow  items-stretch max-height w-full md:w-2/4 text-justify md:text-center">
+        <div
+          className="py-10 md:pb-0 overflow-y-scroll flex-col m-1 flex justify-center md:justify-between items-center p-2 flex-grow md:items-stretch md:w-2/4 text-justify md:text-center w-screen h-screen"
+        >
           <div className="w-full flex justify-center items-center  m-1 p-1">
             <div className="mr-1 text-xs md:text-base md:w-auto">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
               cumque minima facilis eos nesciunt illo ab maxime laborum natus,
               nobis perspiciatis.
             </div>
-            <div className="w-full">
+            <div className="w-full flex justify-center items-center">
               <img src="insta.png" className="w-40" />
             </div>
           </div>
@@ -48,7 +51,7 @@ const WorkDetail = (props) => {
               cumque minima facilis eos nesciunt illo ab maxime laborum natus,
               nobis perspiciatis.
             </div>
-            <div className="w-full">
+            <div className="w-full flex justify-center items-center">
               <img src="insta.png" className="w-40" />
             </div>
           </div>
@@ -58,7 +61,7 @@ const WorkDetail = (props) => {
               cumque minima facilis eos nesciunt illo ab maxime laborum natus,
               nobis perspiciatis.
             </div>
-            <div className="w-full">
+            <div className="w-full flex justify-center items-center">
               <img src="insta.png" className="w-40" />
             </div>
           </div>
@@ -68,7 +71,7 @@ const WorkDetail = (props) => {
               cumque minima facilis eos nesciunt illo ab maxime laborum natus,
               nobis perspiciatis.
             </div>
-            <div className="w-full">
+            <div className="w-full flex justify-center items-center">
               <img src="insta.png" className="w-40" />
             </div>
           </div>
@@ -78,7 +81,7 @@ const WorkDetail = (props) => {
               cumque minima facilis eos nesciunt illo ab maxime laborum natus,
               nobis perspiciatis.
             </div>
-            <div className="w-full">
+            <div className="w-full flex justify-center items-center">
               <img src="insta.png" className="w-40" />
             </div>
           </div>
@@ -88,7 +91,7 @@ const WorkDetail = (props) => {
               cumque minima facilis eos nesciunt illo ab maxime laborum natus,
               nobis perspiciatis.
             </div>
-            <div className="w-full">
+            <div className="w-full flex justify-center items-center">
               <img src="insta.png" className="w-40" />
             </div>
           </div>
--- pages/work.tsx
+++ pages/work.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import Page from "../components/Page/Page";
 import clients from "../data/clients";
-import { workDetail } from "../utils/workDetail";
+import Rodal from "rodal";
+import WorkDetail from "../components/WorkDetail/WorkDetail";
+import "rodal/lib/rodal.css";
 
 interface Client {
   logoPath: string;
@@ -12,133 +14,8 @@ interface Client {
 }
 
 const Work = () => {
-  const [ele, setEle] = useState(null);
-  const [showButton, setShowButton] = useState(null);
-
-  // const details = {
-  //   logoPath: "/logo.svg",
-  // };
-
-  console.log(ele);
-  const toggleExpansion = (element, to, duration = 350) => {
-    return new Promise((res) => {
-      requestAnimationFrame(() => {
-        element.style.transition = `
-          width ${duration}ms ease-in-out,
-          height ${duration}ms ease-in-out,
-          left ${duration}ms ease-in-out,
-          top ${duration}ms ease-in-out
-        `;
-
-        console.log(to);
-
-        element.style.top = to.top;
-        element.style.left = to.left;
-        element.style.width = to.width;
-        element.style.height = to.height;
-        element.style.zIndex = to.zIndex ? to.zIndex : "";
-        element.style.overflow = "scroll";
-        element.style.overflowX = "hidden";
-
-        // element.style.minHeight = to.height === "100vh" ? "100vh" : "";
-      });
-      setTimeout(res, duration);
-    });
-  };
-
-  const fadeContent = (element, opacity, duration = 300) => {
-    return new Promise((res) => {
-      [...element.children].forEach((child) => {
-        requestAnimationFrame(() => {
-          child.style.transition = `opacity ${duration}ms linear`;
-          child.style.opacity = opacity;
-        });
-      });
-      setTimeout(res, duration);
-    });
-  };
-
-  const handleMe = async (e, client) => {
-    setEle(e);
-    console.log(e.currentTarget);
-    const card = e.currentTarget;
-    let cloned = card.cloneNode(true);
-    console.log("cloned", cloned);
-    const { top, left, width, height } = card.getBoundingClientRect();
-
-    cloned.style.position = "fixed";
-    cloned.style.top = top + "px";
-    cloned.style.left = left + "px";
-    cloned.style.width = width + "px";
-    cloned.style.height = height + "px";
-
-    console.log("parent node", card.parentNode.getBoundingClientRect());
-    card.style.opacity = "0";
-
-    card.parentNode.appendChild(cloned);
-    setShowButton({
-      card,
-      cloned,
-      top,
-      left,
-      width,
-      height,
-    });
-    console.log("top", top, "left", left);
-    console.log(cloned);
-
-    fadeContent(cloned, "0").then(() => {
-      [...cloned.children].forEach((child) => (child.style.display = "none"));
-    });
-
-    // const {
-    //   top: parentTop,
-    //   left: parentLeft,
-    //   width: parentWidth,
-    //   height: parentHeight,
-    // } = card.parentNode.getBoundingClientRect();
-
-    await toggleExpansion(cloned, {
-      top: "0px",
-      left: "0px",
-      width: "100vw",
-      height: "100vh",
-      zIndex: "100",
-    });
-
-    const content = workDetail(client);
-
-    cloned.style.display = "block";
-    cloned.style.padding = "0";
-
-    cloned.insertAdjacentHTML("afterbegin", content);
-  };
-
-  const removeFromDom = async () => {
-    const { card, cloned, top, left, width, height } = showButton;
-
-    setShowButton(null);
-
-    cloned.style.removeProperty("display");
-    cloned.style.removeProperty("padding");
-
-    [...cloned.children].forEach((child) => {
-      child.style.removeProperty("display");
-    });
-
-    fadeContent(cloned, "0", 20);
-    //shrink card back
-
-    await toggleExpansion(cloned, {
-      top: `${top}px`,
-      left: `${left}px`,
-      width: `${width}px`,
-      height: `${height}px`,
-    });
-
-    card.style.removeProperty("opacity");
-    cloned.remove();
-  };
+  const [details, setDetails] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
 
   return (
     <Page title="Flamy - Work">
@@ -154,7 +31,10 @@ const Work = () => {
                 className={`group box cursor-pointer bg-opacity-40 ${
                   client.row ? `row-span-${client.row}` : ""
                 } ${client.col ? `col-span-${client.col}` : ""}`}
-                onClick={(e) => handleMe(e, client)}
+                onClick={() => {
+                  setDetails(<WorkDetail />);
+                  setIsVisible(true);
+                }}
               >
                 <div className="content">
                   <img
@@ -169,21 +49,19 @@ const Work = () => {
               </div>
             ))}
           </div>
-
-          <button
-            className={`btn h-10 w-10 top-10 right-5 lg:right-10 border-none focus:outline-none ${
-              !showButton && "hidden"
-            }
-              bg-work
-              rounded-full
-              font-extrabold
-              transition-all
-            `}
-            onClick={removeFromDom}
-          >
-            X
-          </button>
         </div>
+
+        <Rodal
+          visible={isVisible}
+          onClose={() => setIsVisible(false)}
+          className="w-screen h-screen overflow-scroll bg-transparent"
+          width="100vw"
+          height="vh"
+          customStyles={{ backgroundColor: "transparent" }}
+          closeOnEsc={true}
+        >
+          {details}
+        </Rodal>
       </div>
     </Page>
   );
