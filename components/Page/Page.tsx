bd1fdf4a03c99360c22471df822c47ae7e7d26d5
Sameer Utradhi <sameer@flamy.dev>
2020-12-17 10:46:36 +0530

    convert to PWA

--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -9,12 +9,34 @@ const Page = (props) => {
   return (
     <div className="h-full w-full overflow-hidden ">
       <Head>
+        <title>{title}</title>
+
         <meta name="description" content={description} />
         <meta property="og:title" content={title} key="ogtitle" />
         <meta property="og:description" content={description} key="ogdesc" />
         <meta property="og:url" content={currentURL} />
-        <meta property="og:image" content="/logo-square.svg" key="ogimage" />
-        <title>{title}</title>
+        <meta
+          property="og:image"
+          content="https://www.flamy.dev/logo-square.png"
+          key="ogimage"
+        />
+        <meta name="theme-color" content="#333" />
+        <meta name="apple-mobile-web-app-status-bar" content="#333" />
+
+        <link rel="apple-touch-icon" href="/icon-96x96.png" />
+        <link
+          href="/icon-72x72.png"
+          rel="icon"
+          type="image/png"
+          sizes="72x72"
+        />
+        <link
+          href="/icon-96x96.png"
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+        />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="icon" type="image/x-icon" href="/logo-square.svg" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
