eae365f0afada8c3d88e730836389f3b3d5c9dab
Meet Mangukiya <meet@flamy.dev>
2020-12-18 11:48:25 +0530

    attempt to add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,16 @@ export default class CustomDocument extends Document {
           <meta property="custom" content="flamy" />
           <meta charSet="utf-8" />
           <link href="/fonts/style.css" rel="stylesheet" />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-185675449-1"
+          ></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            {/* @ts-ignore */}
+            function gtag(){window.dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'UA-185675449-1');
+          </script>
         </Head>
         <body className="dark ">
           <Main />
