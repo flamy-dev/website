c5ce80d65f4438f27c621af07f128e14c7e768fb
geekysam7 <geekysam7@gmail.com>
2020-12-20 17:19:12 +0530

    add icons

--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -20,10 +20,60 @@ const Page = (props) => {
           content="https://www.flamy.dev/logo-square.png"
           key="ogimage"
         />
-        <meta name="theme-color" content="#333" />
-        <meta name="apple-mobile-web-app-status-bar" content="#333" />
+        <meta name="theme-color" content="#001a66" />
+        <meta name="apple-mobile-web-app-status-bar" content="#001a66" />
 
-        <link rel="apple-touch-icon" href="/icon-96x96.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+        <link
+          href="/icon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
         <link
           href="/icon-72x72.png"
           rel="icon"
