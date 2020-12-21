9674af953e18094ac6e444cc3b9afc3b0a274c69
Meet Mangukiya <meet@flamy.dev>
2020-12-21 13:04:31 +0530

    HiringForm: add processing spinner

--- components/HiringForm.tsx
+++ components/HiringForm.tsx
@@ -75,7 +75,7 @@ const HiringForm = (props) => {
           })}
           onSubmit={() => {
             const data = new FormData(formRef.current);
-            setIsProcessing(true)
+            setIsProcessing(true);
             fetch("/", {
               method: "POST",
               // @ts-ignore
@@ -227,8 +227,33 @@ const HiringForm = (props) => {
                   />
                 </span>
                 <span className="w-1/2">
-                  {/* @ts-ignore */}
-                  <Button type="submit" disabled={isProcessing}>{isProcessing ? "processing..." : "Submit"}</Button>
+                  <Button type="submit" disabled={isProcessing}>
+                    {isProcessing ? (
+                      <>
+                        <svg
+                          className="animate-spin h-5 w-5 mr-3"
+                          viewBox="0 0 24 24"
+                        >
+                          <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                          ></circle>
+                          <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                          ></path>
+                        </svg>
+                        Processing
+                      </>
+                    ) : (
+                      "Submit"
+                    )}
+                  </Button>
                 </span>
               </div>
               <div className="mt-6">
